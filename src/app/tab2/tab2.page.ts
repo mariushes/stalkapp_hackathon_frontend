4949882340b1eeb81e27905a84e766e331d875d8
RalucaChis <48648566+RalucaChis@users.noreply.github.com>
2020-02-24 16:56:09 +0200

    history table

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+  public accounts: Array<string>;
 
   constructor() {}
 
+  ngOnInit() {
+    this.accounts = new Array<string>();
+    this.accounts.push("person1");
+    this.accounts.push("person2");
+    this.accounts.push("person3");
+  }
+
+  addAccount(){
+    // for()
+    //   this.accounts.push();
+  }
 }
